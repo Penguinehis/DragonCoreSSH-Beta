45e73c3d7c04a91bdd320be7d8160d8acc6000e3
Penguinehis <54519344+Penguinehis@users.noreply.github.com>
2024-02-07 00:33:00 -0300

    FIX OPEN

--- iptables.sh
+++ iptables.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 cake="$(ip route get 1 | grep -Po '(?<=dev )(\S+)')"
-iptables -t nat -C POSTROUTING -s "10.8.0.0/24" -o eth0 -j MASQUERADE || iptables -t nat -A POSTROUTING -s "10.8.0.0/24" -o eth0 -j MASQUERADE
+iptables -t nat -C POSTROUTING -s "10.8.0.0/24" -o $cake -j MASQUERADE || iptables -t nat -A POSTROUTING -s "10.8.0.0/24" -o $cake -j MASQUERADE
 iptables -C INPUT -i tun0 -j ACCEPT || iptables -A INPUT -i tun0 -j ACCEPT
 iptables -C INPUT -i lo -j ACCEPT || iptables -A INPUT -i lo -j ACCEPT
 iptables -C INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT || iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
