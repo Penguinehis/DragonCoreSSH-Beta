5f377cc79f720dc2198c3398ba582046cd7c2140
Penguinehis <54519344+Penguinehis@users.noreply.github.com>
2024-04-20 23:18:10 -0300

    Suporte Ubuntu 18

--- install.sh
+++ install.sh
@@ -1,75 +1,94 @@
-system=$(cat /etc/os-release | grep -q 'NAME="Debian GNU/Linux"' && echo debian || echo ubuntu)
+#!/bin/bash
+if grep -q 'NAME="Debian GNU/Linux"' /etc/os-release; then
+    system="debian"
+else
+    system="ubuntu"
+fi
+
 if [ "$system" = "debian" ]; then
-apt-get install sudo
+    apt-get install -y sudo
+fi
+
 sudo apt update
 sudo apt install -y lsb-release ca-certificates apt-transport-https software-properties-common gnupg curl wget
-repos2=$(find /etc/apt/ -name *.list | xargs cat | grep  ^[[:space:]]*deb | grep -q "packages.sury.org/php" && echo 1 || echo 0)
-if [ "$repos2" = "1" ]; then
-echo "OK"   
+
+if [ "$system" = "debian" ]; then
+    repos=$(find /etc/apt/ -name '*.list' -exec cat {} + | grep  ^[[:space:]]*deb | grep -q "packages.sury.org/php" && echo 1 || echo 0)
+    if [ "$repos" = "0" ]; then
+        echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/sury-php.list
+        curl -fsSL  https://packages.sury.org/php/apt.gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/sury-keyring.gpg
+        sudo apt update
+    fi
 else
-echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/sury-php.list
-curl -fsSL  https://packages.sury.org/php/apt.gpg| sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/sury-keyring.gpg
-apt update
+    repos=$(find /etc/apt/ -name '*.list' -exec cat {} + | grep  ^[[:space:]]*deb | grep -q "/ondrej/php" && echo 1 || echo 0)
+    if [ "$repos" = "0" ]; then
+        sudo apt install lsb-release ca-certificates apt-transport-https software-properties-common -y
+        sudo add-apt-repository ppa:ondrej/php
+        sudo apt update
+    fi
 fi
-else
-reposi=$(find /etc/apt/ -name *.list | xargs cat | grep  ^[[:space:]]*deb | grep -q "/ondrej/php" && echo 1 || echo 0)
-if [ "$reposi" = "1" ]; then
-echo "OK"   
-else
-    apt install lsb-release ca-certificates apt-transport-https software-properties-common -y
-    add-apt-repository ppa:ondrej/php
-    apt update
-fi
-fi
-php_version2="$(command php --version 2>'/dev/null' \
+
+php_version="$(command php --version 2>'/dev/null' \
 | command head -n 1 \
 | command cut --characters=5-7)"
-if [ "$php_version2" != "8.1" ]; then
-apt update
-apt purge php-cli php-curl php-sqlite3 -y
-apt purge php8.2-cli php8.2-curl php8.2-sqlite3 git -y
-apt autoremove -y
-apt install php8.1-cli php8.1-curl php8.1-sqlite3 php8.1-pgsql git -y
-else
-apt update
-apt install php8.1-cli php8.1-curl php8.1-sqlite3 php8.1-pgsql git -y
-fi
-PREFERENCES_FILE="/etc/apt/preferences.d/php-pin-8.1.pref"
-if [ -f "$PREFERENCES_FILE" ]; then
-    echo "Preferences file already exists. Exiting."
-fi
-tee "$PREFERENCES_FILE" <<EOF
+if [ "$system" = "ubuntu" ] && [ "$(lsb_release -rs)" = "18.04" ]; then
+    sudo apt install php7.2-cli php7.2-curl php7.2-sqlite3 php7.2-pgsql git -y
+    wget --user-agent "Mozilla" http://www.sourceguardian.com/loaders/download/loaders.linux-x86_64.tar.gz
+    tar -xvzf loaders.linux-x86_64.tar.gz
+    sudo mv ixed.7.2.lin $(php -i | grep extension_dir | awk '{print $3}' | head -n 1)
+    rm loaders.linux-x86_64.tar.gz
+elif [ "$php_version" != "8.1" ]; then
+    sudo apt purge php-cli php-curl php-sqlite3 php-pgsql -y
+    sudo apt purge php8.2-cli php8.2-curl php8.2-sqlite3 git -y
+    sudo apt autoremove -y
+    sudo apt install php8.1-cli php8.1-curl php8.1-sqlite3 php8.1-pgsql git -y
+    sudo update-alternatives --set php /usr/bin/php8.1
+
+    PREFERENCES_FILE="/etc/apt/preferences.d/php-pin-8.1.pref"
+    if [ ! -f "$PREFERENCES_FILE" ]; then
+        sudo tee "$PREFERENCES_FILE" <<EOF
 Package: php*
 Pin: version 8.1*
 Pin-Priority: 1001
 EOF
-apt update
-apt upgrade -y
-apt install screen htop nload lsof curl -y
-update-alternatives --set php /usr/bin/php8.1
-cake=$(uname -m)
-if [ "$cake" = "x86_64" ]; then
-curl -O https://downloads.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.gz
-tar -xvzf ioncube_loaders_lin_x86-64.tar.gz
-rm ioncube_loaders_lin_x86-64.tar.gz
+        sudo apt update
+        sudo apt upgrade -y
+    fi
+    wget --user-agent "Mozilla" https://www.sourceguardian.com/loaders/download/loaders.linux-x86_64.tar.gz
+    tar -xvzf loaders.linux-x86_64.tar.gz
+    sudo mv ixed.${php_version}.lin $(php -i | grep extension_dir | awk '{print $3}' | head -n 1)
+    rm loaders.linux-x86_64.tar.gz
 else
-curl -O https://downloads.ioncube.com/loader_downloads/ioncube_loaders_lin_aarch64.tar.gz
-tar -xvzf ioncube_loaders_lin_aarch64.tar.gz
-rm ioncube_loaders_lin_aarch64.tar.gz
-fi
-cd ioncube
-php_ext_dir="$(command php -i | grep extension_dir 2>'/dev/null' \
-| command head -n 1 \
-| command cut --characters=31-38)"
-php_version="$(command php --version 2>'/dev/null' \
-| command head -n 1 \
-| command cut --characters=5-7)"
-cp ioncube_loader_lin_${php_version}.so /usr/lib/php/${php_ext_dir}
-cd ..
-rm -rf ioncube
-cat > /etc/php/${php_version}/cli/conf.d/00-ioncube-loader.ini << EOF
-zend_extension=ioncube_loader_lin_${php_version}.so
+    sudo apt install php8.1-cli php8.1-curl php8.1-sqlite3 php8.1-pgsql git -y
+    sudo update-alternatives --set php /usr/bin/php8.1
+
+    PREFERENCES_FILE="/etc/apt/preferences.d/php-pin-8.1.pref"
+    if [ ! -f "$PREFERENCES_FILE" ]; then
+        sudo tee "$PREFERENCES_FILE" <<EOF
+Package: php*
+Pin: version 8.1*
+Pin-Priority: 1001
 EOF
+        sudo apt update
+        sudo apt upgrade -y
+    fi
+    wget --user-agent "Mozilla" https://www.sourceguardian.com/loaders/download/loaders.linux-x86_64.tar.gz
+    tar -xvzf loaders.linux-x86_64.tar.gz
+    sudo mv ixed.${php_version}.lin $(php -i | grep extension_dir | awk '{print $3}' | head -n 1)
+    rm loaders.linux-x86_64.tar.gz
+fi
+
+cat > /etc/php/${php_version}/cli/conf.d/sourceguardian.ini << EOF
+zend_extension=ixed.${php_version}.lin
+EOF
+rm ixed.*
+rm README
+rm "SourceGuardian Loader License.pdf"
+rm version
+
+if [ ! -e "/bin/php" ]; then
+    sudo ln -s "$(command -v php)" /bin/php
+fi
 
 cd /opt/
 rm -rf DragonCore
